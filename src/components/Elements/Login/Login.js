3631bcb84d8e4caa4fa8b0006188c48d66babf4f
priyarajput97 <priya@excellencetechnologies.info>
2020-04-19 17:58:00 +0530

    login page added

--- src/components/Elements/Login/Login.js
+++ src/components/Elements/Login/Login.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Login extends Component {
     render() {
         return (
-            <div>
-                Login
+            <div className="login">
+                <div className="login-box">
+                    <h1 className="login-box-heading">Ditchcord</h1>
+                    <div className="login-box-divider"></div>
+                    <button className="btn btn-dark">
+                        <i className="fab fa-google mr-3"></i>
+                        Login with Google
+                    </button>
+                </div>
             </div>
         );
     }
